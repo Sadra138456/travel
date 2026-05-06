3f2b5db97c73b44c16912517d52c84cff18c6ab0
sadra cheraghi <162758424+Sadra138456@users.noreply.github.com>
2026-05-06 19:58:23 +0330

    Update client_transport.go

--- client_transport.go
+++ client_transport.go
@@ -11,76 +11,113 @@ import (
 )
 
 type ClientTransport struct {
-	cfg        *ClientConfig
-	enc        *ClientQuantumEncryption
-	quicConn   quic.Connection
-	httpsConn  net.Conn
-	vncConn    net.Conn
+	cfg  *ClientConfig
+	enc  *ClientQuantumEncryption
+	conn net.Conn
+	ctx  context.Context
 }
 
-func NewClientTransport(cfg *ClientConfig, enc *ClientQuantumEncryption) (*ClientTransport, error) {
+func NewClientTransport(cfg *ClientConfig, enc *ClientQuantumEncryption) *ClientTransport {
 	return &ClientTransport{
 		cfg: cfg,
 		enc: enc,
-	}, nil
+		ctx: context.Background(),
+	}
 }
 
-func (t *ClientTransport) SendQUIC(data []byte) error {
-	if t.quicConn == nil {
-		tlsConfig := &tls.Config{
-			InsecureSkipVerify: true,
-			NextProtos:         []string{"h3"},
-		}
-		
-		addr := fmt.Sprintf("%s:%d", t.cfg.Server.Address, t.cfg.Server.Port)
-		conn, err := quic.DialAddr(context.Background(), addr, tlsConfig, nil)
-		if err != nil {
-			return err
-		}
-		t.quicConn = conn
+func (t *ClientTransport) ConnectQUIC() error {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"h3"},
 	}
 
-	stream, err := t.quicConn.OpenStreamSync(context.Background())
+	addr := fmt.Sprintf("%s:%d", t.cfg.ServerAddr, t.cfg.QUICPort)
+	conn, err := quic.DialAddr(t.ctx, addr, tlsConfig, nil)
 	if err != nil {
 		return err
 	}
-	defer stream.Close()
 
-	stream.SetWriteDeadline(time.Now().Add(5 * time.Second))
-	_, err = stream.Write(data)
-	return err
-}
-
-func (t *ClientTransport) SendHTTPS(data []byte) error {
-	if t.httpsConn == nil {
-		tlsConfig := &tls.Config{
-			InsecureSkipVerify: true,
-		}
-		
-		addr := fmt.Sprintf("%s:%d", t.cfg.Server.Address, t.cfg.Server.Port)
-		conn, err := tls.Dial("tcp", addr, tlsConfig)
-		if err != nil {
-			return err
-		}
-		t.httpsConn = conn
+	stream, err := conn.OpenStreamSync(t.ctx)
+	if err != nil {
+		return err
 	}
 
-	t.httpsConn.SetWriteDeadline(time.Now().Add(5 * time.Second))
-	_, err := t.httpsConn.Write(data)
-	return err
+	t.conn = &quicStreamConn{stream, conn}
+	return nil
 }
 
-func (t *ClientTransport) SendVNC(data []byte) error {
-	if t.vncConn == nil {
-		addr := fmt.Sprintf("%s:%d", t.cfg.Server.Address, t.cfg.Server.Port)
-		conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
-		if err != nil {
-			return err
-		}
-		t.vncConn = conn
+func (t *ClientTransport) ConnectHTTPS() error {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
 	}
 
-	t.vncConn.SetWriteDeadline(time.Now().Add(5 * time.Second))
-	_, err := t.vncConn.Write(data)
-	return err
+	addr := fmt.Sprintf("%s:%d", t.cfg.ServerAddr, t.cfg.HTTPSPort)
+	conn, err := tls.Dial("tcp", addr, tlsConfig)
+	if err != nil {
+		return err
+	}
+
+	t.conn = conn
+	return nil
+}
+
+func (t *ClientTransport) ConnectVNC() error {
+	addr := fmt.Sprintf("%s:%d", t.cfg.ServerAddr, t.cfg.VNCPort)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 12)
+	conn.Read(buf)
+
+	t.conn = conn
+	return nil
+}
+
+func (t *ClientTransport) Write(data []byte) (int, error) {
+	if t.conn == nil {
+		return 0, fmt.Errorf("not connected")
+	}
+	encrypted := t.enc.Encrypt(data)
+	return t.conn.Write(encrypted)
+}
+
+func (t *ClientTransport) Read(buf []byte) (int, error) {
+	if t.conn == nil {
+		return 0, fmt.Errorf("not connected")
+	}
+	n, err := t.conn.Read(buf)
+	if err != nil {
+		return n, err
+	}
+	decrypted := t.enc.Decrypt(buf[:n])
+	copy(buf, decrypted)
+	return len(decrypted), nil
+}
+
+func (t *ClientTransport) Close() error {
+	if t.conn != nil {
+		return t.conn.Close()
+	}
+	return nil
+}
+
+type quicStreamConn struct {
+	quic.Stream
+	conn quic.Connection
+}
+
+func (q *quicStreamConn) LocalAddr() net.Addr {
+	return q.conn.LocalAddr()
+}
+
+func (q *quicStreamConn) RemoteAddr() net.Addr {
+	return q.conn.RemoteAddr()
+}
+
+func (q *quicStreamConn) SetDeadline(t time.Time) error {
+	q.SetReadDeadline(t)
+	q.SetWriteDeadline(t)
+	return nil
 }
